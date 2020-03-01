0dc02fe69d484696d178161092a269b49152e697
meox92@gmail.com <meox92@gmail.com>
2020-03-01 10:39:02 +0100

    production code for signupWithReferral

---
+++ SignupApp/UserUseCase.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  SignupApp
+//
+//  Created by Maola Ma on 01/03/2020.
+//  Copyright © 2020 Maola. All rights reserved.
+//
+import Cocoa
+
+protocol UserServices {
+    func signup(with mail: String, password: String, completion: (Result<User, Error>) -> Void)
+    func validateCode(code: String, user: User, completion: @escaping(Result<User, Error>) -> Void)
+}
+
+struct User {
+    let email: String
+    let invitationCode: String?
+}
+
+class UserUseCase {
+    private var userServices: UserServices
+    
+    init(userServices: UserServices) {
+        self.userServices = userServices
+    }
+    
+    func signupWithReferralCode(email: String, password: String, referral: String, completion: @escaping (User) -> Void) {
+        userServices.signup(with: email, password: password) { result in
+            switch result {
+            case .success(let user):
+                self.userServices.validateCode(code: referral, user: user) { result in
+                   if let updatedUser = try? result.get() {
+                       completion(updatedUser)
+                   }
+                }
+            case .failure:
+                break
+            }
+
+        }
+
+    }
+}
+
+
+
+
